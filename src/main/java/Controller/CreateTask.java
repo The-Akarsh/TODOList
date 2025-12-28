f8a05d021f5156ae97fb151cf566944444cac9e8
Akarsh <akarshajith06@gmail.com>
2025-12-28 15:09:59 +0530

    Updated some backend

--- src/main/java/Controller/CreateTask.java
+++ src/main/java/Controller/CreateTask.java
@@ -2,13 +2,22 @@ package Controller;
 
 import Model.Task;
 import Model.TaskList;
+import View.TaskUI;
 
 import java.time.LocalDateTime;
 
 public class CreateTask {
-    public static void create(String name, String description, int priority, String dateTime){
+    /** Used for taking input from fields of  <code>View.TaskUI</code>. Create and add object of type Task to adds them to array list of
+     * type Task in <code>Model.TaskList */
+    public static void create(TaskUI taskUI){
 
-        LocalDateTime deadline = HandleDateTime.stringTOLocalDateTime(dateTime);
+        String name = taskUI.getName();
+        String description = taskUI.getDescription();
+        int priority = taskUI.getPriority();
+        String dateTime = taskUI.getDateTime();
+        LocalDateTime deadline = null;
+        if(dateTime != null)
+            deadline = HandleDateTime.stringTOLocalDateTime(dateTime);
         Task newTask = new Task(name, description, priority, deadline);
         TaskList.taskList.add(newTask);
     }
--- src/main/java/Controller/HandleDateTime.java
+++ src/main/java/Controller/HandleDateTime.java
@@ -4,17 +4,18 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.text.SimpleDateFormat;
-
+/** Created in <code>Model.HandleDateTime</code>. Class contains tools for handling date & time of data type LocalDateTime and String.
+ *  dateTimeFormatter variable is used to define pattern */
 public class HandleDateTime {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-    private static final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-    private static final DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("h:mm a dd/MM/yyyy");
+    public static final DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("h:mm a dd/MM/yyyy");
+    /** Created in <code>Model.HandleDateTime</code> class.Takes Date data type and String time as input and
+     *  returns String in dateTimeFormat(self defined) format */
     public static String rawTimeTOSring(Date date, String time){
         String newDate = dateFormat.format(date);
         return time + " " + newDate;
     }
-    /**
-     * Takes String data type as input and returns LocalDateTime data type in dateTimeFormat(self defined) format */
+    /**Takes String data type as input and returns LocalDateTime data type in dateTimeFormat(self defined) format */
     public static LocalDateTime stringTOLocalDateTime(String stringTime){
         return LocalDateTime.parse(stringTime,dateTimeFormat);
     }
